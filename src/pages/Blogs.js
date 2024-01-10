bdc9bff05cc726433c0df9dedfac8c466a25124b
RohanSharma4792 <rohan.sharma2020@vitbhopal.ac.in>
2024-01-10 12:19:37 +0530

    missed commit

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -4,6 +4,7 @@ const Blogs = () => {
         <>
         <div className={styles.blogss}>
             <h1 className={styles.heading}>This is javascript</h1>
+            
             <p className={styles.para}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti inventore accusamus quis reiciendis, iste repudiandae quam, amet quia perspiciatis veritatis cupiditate, sit est?</p>
             <h1 className={styles.heading}>This is typescript</h1>
             <p className={styles.para}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti inventore accusamus quis reiciendis, iste repudiandae quam, amet quia perspiciatis veritatis cupiditate, sit est?</p>
